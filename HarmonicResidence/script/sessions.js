58163eac0092fb167b58d3345b7d1f2a39047db6
weiting t lu <weiting.t.lu@gmail.com>
2012-03-01 12:59:07 -0800

    added stubs for groups and sessions

---
+++ HarmonicResidence/script/sessions.js
@@ -0,0 +1,11 @@
+/*
+ *
+ * Harmonic Residence Session Controller
+ */
+ 
+ 
+//Post: Create a Music Session based on the given group_id
+function createSession(name, description, group_id) {
+	//Call our own server
+	//Get back a session id
+} 
