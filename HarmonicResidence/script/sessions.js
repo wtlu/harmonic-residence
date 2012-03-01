866cdf6dbc5f5c22eae858677b6fb7697595eedc
weiting t lu <weiting.t.lu@gmail.com>
2012-03-01 13:16:36 -0800

    added stubs for ending session

--- HarmonicResidence/script/sessions.js
+++ HarmonicResidence/script/sessions.js
@@ -9,3 +9,9 @@ function createSession(name, description, group_id) {
 	//Call our own server
 	//Get back a session id
 } 
+
+//Post: Ends a session based on the given session id
+function endSession(session_id) {
+	//End a session
+	//Now call our server
+}
