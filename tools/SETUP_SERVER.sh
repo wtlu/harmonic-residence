a732fb5ddcf1b271cb2f056308d55a03aad3068a
weiting t lu <weiting.t.lu@gmail.com>
2012-03-06 00:56:13 -0800

    fixed bug where script is stuck in super user mode and will not do rest of commands

--- tools/SETUP_SERVER.sh
+++ tools/SETUP_SERVER.sh
@@ -4,7 +4,7 @@
 # Purpose	: This script quickly gets the tar.gz files for the server
 #				unloads it in /var/www/html
 
-sudo su
+#Make sure you are super user before running!
 cd /var/www/html/
 rm -rf *
 wget http://harmonic-residence.googlecode.com/hg/harmoniius-server/harmoniius-server.tar.gz
