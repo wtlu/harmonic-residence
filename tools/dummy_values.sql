3b6e97c38e39d725bb9cc5ab8884bba4b258fa8f
weiting t lu <weiting.t.lu@gmail.com>
2012-03-07 22:10:28 -0800

    created dummy values sql file for testing

---
+++ tools/dummy_values.sql
@@ -0,0 +1,8 @@
+use media_db;
+
+INSERT INTO users(id, name, facebook_id, created)
+	VALUES(1, "Wei-Ting Lu", 518118311, NOW());
+	
+INSERT INTO users(id, name, facebook_id, created)
+	VALUES(1, "Shiho Asanuma", 518118311, NOW());
+	
