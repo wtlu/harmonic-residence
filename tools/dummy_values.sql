1b883bf6a8a33238e6c41b8326e91cbee5f17406
weiting t lu <weiting.t.lu@gmail.com>
2012-03-07 22:14:22 -0800

    fixed bug in dummy values

--- tools/dummy_values.sql
+++ tools/dummy_values.sql
@@ -4,5 +4,5 @@ INSERT INTO users(id, name, facebook_id, created)
 	VALUES(1, "Wei-Ting Lu", 518118311, NOW());
 	
 INSERT INTO users(id, name, facebook_id, created)
-	VALUES(1, "Shiho Asanuma", 518118311, NOW());
+	VALUES(2, "Shiho Asanuma", 518118311, NOW());
 	
